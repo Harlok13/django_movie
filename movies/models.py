e3408fd28de5f05973bf6445e754ad164a8119c4
Harlok <holmik90@yandex.ru>
2022-11-08 12:47:31 +0300

    add model Actor

--- movies/models.py
+++ movies/models.py
@@ -14,6 +14,21 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
 
+class Actor(models.Model):
+    """Актёры и режиссёры"""
+    name = models.CharField('Имя', max_length=100)
+    age = models.PositiveSmallIntegerField('Возраст', default=0)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='actors/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Актёры и режиссёры'
+        verbose_name_plural = 'Актёры и режиссёры'
+
+
 
 
 
