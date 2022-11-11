df7e6463f3c45e86d724e392d52d8cbf4e558c66
Harlok <holmik90@yandex.ru>
2022-11-11 22:43:09 +0300

    add static

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    # '/var/www/static/',
 ]
 
 MEDIA_URL = '/media/'
