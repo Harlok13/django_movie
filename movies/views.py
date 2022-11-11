2fc0d7006f4c63e8cb7e89b936b56349bf3f6fc3
Harlok <holmik90@yandex.ru>
2022-11-11 16:30:10 +0300

    create movie_list.html

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
+import movies as movies
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from movies.models import Movie
+
+
+class MoviesView(View):
+    """Список фильмов"""
+    def get(self, request,):
+        movies = Movie.objects.all()
+        return render(request, "movies/movie_list.html", {"movie_list": movies})
